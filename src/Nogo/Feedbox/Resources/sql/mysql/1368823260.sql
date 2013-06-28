ed363741f8acc2e4837193c9914c29a1ffa91323
Danilo Kuehn <dk@nogo-software.de>
2013-06-28 14:53:39 +0200

    update mysql migrations

--- src/Nogo/Feedbox/Resources/sql/mysql/1368823260.sql
+++ src/Nogo/Feedbox/Resources/sql/mysql/1368823260.sql
@@ -1,28 +1,30 @@
-CREATE TABLE IF NOT EXISTS sources (
-  id INTEGER PRIMARY KEY,
-  name TEXT NOT NULL,
-  uri TEXT NOT NULL,
-  icon TEXT,
-  active INTEGER DEFAULT 1,
-  unread INTEGER DEFAULT 0,
-  errors TEXT,
-  period TEXT,
-  last_update DATETIME,
-  created_at DATETIME,
-  updated_at DATETIME
-);
+CREATE TABLE IF NOT EXISTS `sources` (
+  `id` int(11) AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `uri` text NOT NULL,
+  `icon` text,
+  `active` tinyint(1) DEFAULT 1,
+  `unread` int(11) DEFAULT 0,
+  `errors` text,
+  `period` text,
+  `last_update` datetime,
+  `created_at` datetime,
+  `updated_at` datetime,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS items (
-  id INTEGER PRIMARY KEY,
-  source_id INTEGER,
-  `read` DATETIME DEFAULT NULL,
-  starred INTEGER DEFAULT 0,
-  title TEXT NOT NULL,
-  pubdate DATETIME NOT NULL,
-  content TEXT NOT NULL,
-  uid TEXT NOT NULL,
-  uri TEXT NOT NULL,
-  created_at DATETIME,
-  updated_at DATETIME,
-  FOREIGN KEY (source_id) REFERENCES sources(id)
-);
+CREATE TABLE IF NOT EXISTS `items` (
+  `id` int(11) AUTO_INCREMENT,
+  `source_id` int(11),
+  `read` datetime DEFAULT NULL,
+  `starred` tinyint(1) DEFAULT 0,
+  `title` text NOT NULL,
+  `pubdate` datetime NOT NULL,
+  `content` text NOT NULL,
+  `uid` text NOT NULL,
+  `uri` text NOT NULL,
+  `created_at` datetime,
+  `updated_at` datetime,
+  PRIMARY KEY (`id`),
+  CONSTRAINT FOREIGN KEY (`source_id`) REFERENCES `sources` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB;
--- src/Nogo/Feedbox/Resources/sql/mysql/1372086499.sql
+++ src/Nogo/Feedbox/Resources/sql/mysql/1372086499.sql
@@ -1,28 +1,37 @@
-CREATE TABLE IF NOT EXISTS version (
-  key VARCHAR(255) PRIMARY KEY
-);
+CREATE TABLE IF NOT EXISTS `version` (
+  `key` varchar(255),
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB;
 
-INSERT INTO version VALUES ('1368823260');
-INSERT INTO version VALUES ('1372086499');
+INSERT INTO `version` VALUES ('1368823260');
+INSERT INTO `version` VALUES ('1372086499');
 
-CREATE TABLE IF NOT EXISTS setting (
-  id INTEGER PRIMARY KEY,
-  key VARCHAR(255) NOT NULL,
-  value TEXT,
-  created_at DATETIME,
-  updated_at DATETIME
-);
+CREATE TABLE IF NOT EXISTS `settings` (
+  `id` int(11) AUTO_INCREMENT,
+  `key` varchar(255) NOT NULL,
+  `value` text,
+  `created_at` datetime,
+  `updated_at` datetime,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS tags (
-  id INTEGER PRIMARY KEY,
-  name varchar(255) NOT NULL UNIQUE,
-  color VARCHAR(20) NOT NULL DEFAULT '#000000',
-  unread INTEGER DEFAULT 0,
-  created_at DATETIME,
-  updated_at DATETIME
-);
+INSERT INTO `settings` (`key`, `value`, `created_at`, `updated_at`) VALUES ('view.unread.sortby', 'oldest', 'NOW()', 'NOW()');
+INSERT INTO `settings` (`key`, `value`, `created_at`, `updated_at`) VALUES ('view.unread.count', '50', 'NOW()', 'NOW()');
+INSERT INTO `settings` (`key`, `value`, `created_at`, `updated_at`) VALUES ('view.read.sortby', 'newest', 'NOW()', 'NOW()');
+INSERT INTO `settings` (`key`, `value`, `created_at`, `updated_at`) VALUES ('view.read.count', '50', 'NOW()', 'NOW()');
+INSERT INTO `settings` (`key`, `value`, `created_at`, `updated_at`) VALUES ('view.starred.sortby', 'newest', 'NOW()', 'NOW()');
+INSERT INTO `settings` (`key`, `value`, `created_at`, `updated_at`) VALUES ('view.starred.count', '50', 'NOW()', 'NOW()');
 
-CREATE TABLE IF NOT EXISTS source_tags (
-  source_id INTEGER NOT NULL,
-  tag_id INTEGER NOT NULL
-);
+
+CREATE TABLE IF NOT EXISTS `tags` (
+  `id` int(11) AUTO_INCREMENT ,
+  `name` varchar(255) NOT NULL UNIQUE,
+  `color` varchar(20),
+  `unread` int(11) DEFAULT 0,
+  `created_at` datetime,
+  `updated_at` datetime,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+ALTER TABLE `sources` ADD COLUMN `tag_id` int(11);
+ALTER TABLE `sources` ADD CONSTRAINT `fk_source_tag` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE SET NULL;
